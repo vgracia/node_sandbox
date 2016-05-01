08ba05adde204e32ad077896290cbbdebcd050c1
Vinson Gracia <vinson@planmatcher.com>
2016-05-01 13:41:18 -0500

    SSR: adding state - bad example

--- serverside-react/views/index.jsx
+++ serverside-react/views/index.jsx
@@ -17,7 +17,7 @@ class TodoList extends React.Component {
             <div className="todoList">
                 <table style={{border: "2px solid black"}}>
                     <tbody>
-                    <Todo title="Shopping">123</Todo>
+                    <Todo title="Shopping">Milk</Todo>
                     <Todo title="Hair cut">13:00</Todo>
                     <Todo title="Learn React">15:00</Todo>
                     </tbody>
@@ -28,9 +28,21 @@ class TodoList extends React.Component {
 }
 
 class Todo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {checked: false};
+	}
+
+	handleChange(e) {
+		this.setState({checked: e.target.checked});
+	}
+
 	render() {
 		return (
 			<tr>
+				<td style={{border: "1px solid black"}}>
+					<input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+				</td>
 				<td style={{border: "1px solid black"}}>{this.props.title}</td>
 				<td style={{border: "1px solid black"}}>{this.props.children}</td>
 			</tr>
