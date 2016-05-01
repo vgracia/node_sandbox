34856246bdec9479d2acb8867b53cf9265cac722
Vinson Gracia <vinson@planmatcher.com>
2016-05-01 12:49:32 -0500

    SSR: properties

--- serverside-react/views/index.jsx
+++ serverside-react/views/index.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 
 export default class TodoBox extends React.Component {
+        render() {
+            return (
+                <div className="todoBox">
+                    <h1>Todos</h1>
+                    <TodoList />
+                    <TodoForm />
+                </div>
+            );
+        }}
+
+class TodoList extends React.Component {
     render() {
         return (
-            <div className="todoBox">
-                <h1>Todos</h1>
-                <TodoList />
-                <TodoForm />
+            <div className="todoList">
+                <table style={{border: "2px solid black"}}>
+                    <tbody>
+                    <Todo title="Shopping">Milk</Todo>
+                    <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
             </div>
         );
     }
 }
 
-class TodoList extends React.Component {
+class Todo extends React.Component {
 	render() {
 		return (
-			<div className="todoList">
-				I am a TodoList.
-			</div>
+			<tr>
+				<td style={{border: "1px solid black"}}>{this.props.title}</td>
+				<td style={{border: "1px solid black"}}>{this.props.children}</td>
+			</tr>
 		);
 	}
 }
@@ -26,8 +41,9 @@ class TodoForm extends React.Component {
 	render() {
 		return (
 			<div className="todoForm">
-				I am a TodoForm.
+			I am a TodoForm.
 			</div>
 		);
+
 	}
 }
